139001ff994ea0a8fbaf934623c38c2d8111b6e9
jjaassoonn <jujian.zhang1998@outlook.com>
2025-01-21 03:11:30 +0000

    universal

--- Project/GR/Basic.lean
+++ Project/GR/Basic.lean
@@ -1,7 +1,8 @@
 import Mathlib.Algebra.PresentedMonoid.Basic
 import Mathlib.Tactic.Group
+import Mathlib.Tactic.ApplyFun
 
-universe u
+universe u v
 
 variable (M : Type u) [CommMonoid M]
 
@@ -119,15 +120,48 @@ lemma coe_same (x : M) : (↑(x, x) : Mᵍʳ) = 1 := by
   simp
 
 @[to_additive]
-instance : MulOneClass Mᵍʳ where
+instance : Group Mᵍʳ where
   one_mul a := recOn a (by simp) (by simp) (by simp)
   mul_one a := recOn a (by simp) (by simp) (by simp)
-
-
-@[to_additive]
-instance : Group Mᵍʳ where
   inv_mul_cancel a := by
     obtain ⟨⟨a, b⟩, rfl⟩ := Con.mk'_surjective a
     simp [← Con.coe_mul, mul_comm a]
+
+@[to_additive]
+def lift {G : Type v} [Group G] (f : M →* G) : Mᵍʳ →* G :=
+  Con.lift _
+    { toFun p := (f p.1) * (f p.2)⁻¹
+      map_one' := by
+        simp only [Prod.fst_one, map_one, Prod.snd_one, inv_one, mul_one]
+      map_mul' := by
+        rintro ⟨a, b⟩ ⟨c, d⟩
+        simp only [Prod.mk_mul_mk, map_mul, mul_inv_rev, ← mul_assoc, mul_inv_eq_iff_eq_mul]
+        simp only [mul_assoc, mul_right_inj]
+        symm
+        simp only [← map_mul, mul_comm b, inv_mul_eq_iff_eq_mul]
+        simp [map_mul]  } <| by
+    rintro ⟨x, y⟩ ⟨x', y'⟩ ⟨z, h⟩
+    have eq₀ := congr(f $h)
+    simp only [map_mul, mul_left_inj, Con.ker_rel, MonoidHom.coe_mk, OneHom.coe_mk] at eq₀ ⊢
+    rw [mul_inv_eq_iff_eq_mul, mul_assoc, show (f y')⁻¹ * f y = f y * (f y')⁻¹ by
+      rw [inv_mul_eq_iff_eq_mul, ← mul_assoc, ← map_mul, mul_comm y', eq_comm,
+        mul_inv_eq_iff_eq_mul, map_mul], ← mul_assoc, eq_comm, mul_inv_eq_iff_eq_mul]
+    exact eq₀.symm
+
+@[to_additive (attr := simp)]
+lemma lift_comp_emb {G : Type v} [Group G] (f : M →* G) : (lift f).comp (emb M) = f := by
+  ext x
+  simp [lift, emb]
+
+@[to_additive (attr := simp)]
+lemma lift_uniq {G : Type v} [Group G] (f : M →* G) (f' : Mᵍʳ →* G) (h : f'.comp (emb M) = f) :
+    f' = lift f := by
+  ext x
+  obtain ⟨⟨a, b⟩, rfl⟩ := Con.mk'_surjective x
+  simp only [Con.coe_mk', lift, Con.lift_coe, MonoidHom.coe_mk, OneHom.coe_mk]
+  have eq (x : M) := congr($h x)
+  simp only [emb, MonoidHom.coe_comp, MonoidHom.coe_mk, OneHom.coe_mk, Function.comp_apply] at eq
+  rw [show ((a, b) : M × M) = (a, 1) * (1, b) by simp, Con.coe_mul, map_mul, eq,
+    show (↑((1, b) : M × M) : Mᵍʳ) = ↑(((b, 1) : M × M) : Mᵍʳ)⁻¹ by simp, map_inv, eq]
 
 end GR
