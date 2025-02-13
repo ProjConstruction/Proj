d7f3fe1d20438173a8e298c3b39e8bd2814f75f1
jjaassoonn <jujian.zhang1998@outlook.com>
2025-02-13 01:18:37 +0000

    by @rndmx

--- Project/Dialation/Multicenter.lean
+++ Project/Dialation/Multicenter.lean
@@ -19,65 +19,88 @@ namespace Multicenter
 
 section semiring
 
-variable {A : Type*} [CommSemiring A] (M : Multicenter A)
+variable {A : Type*} [CommSemiring A] (F : Multicenter A)
 
-scoped notation:max M"^ℕ" => DirectSum (Multicenter.index M) (fun _ => ℕ)
+scoped notation: max F"^ℕ"  => Multicenter.index F  →₀ ℕ
 
-abbrev powIndex : Type := ⨁ _ : M.index, ℕ
+abbrev powIndex : Type := ⨁ _ : F.index, ℕ
 
-def shiftedIdeal (i : M.index) : Ideal A := M.ideal i + Ideal.span {M.elem i}
+def LargeIdeal (i : F.index) : Ideal A := F.ideal i + Ideal.span {F.elem i}
 
-abbrev sumShiftedIdealPower (v : M^ℕ) : Ideal A :=
-    ⨆ i : M.index, (M.shiftedIdeal i)^(v i)
+abbrev prodLargeIdealPower (v : F^ℕ) : Ideal A :=
+    ∏ i ∈ v.support, (F.LargeIdeal i)^(v i)
 
-local prefix:max "𝐋^" => sumShiftedIdealPower M
+local prefix:max "𝐋^" => prodLargeIdealPower F
 
-variable [DecidableEq M.index]
-abbrev sumElemPower (v : M^ℕ) : A := ∏ i ∈ v.support, M.elem i ^ v i
+variable [DecidableEq F.index]
+abbrev prodElemPower (v : F^ℕ) : A := v.prod fun i k ↦ F.elem i ^ k
 
-local prefix:max "𝐚^" => sumElemPower M
+local prefix:max "𝐚^" => prodElemPower F
 
-structure PreDialation where
-  pow : M^ℕ
+structure PreDil where
+  pow : F^ℕ
   num : A
   num_mem : num ∈ 𝐋^pow
 
-def r : M.PreDialation → M.PreDialation → Prop := fun x y =>
-  ∃ β : M^ℕ, x.num * 𝐚^(β + y.pow) = y.num * 𝐚^(β + x.pow)
+def r : F.PreDil → F.PreDil → Prop := fun x y =>
+  ∃ β : F^ℕ, x.num * 𝐚^(β + y.pow) = y.num * 𝐚^(β + x.pow)
 
-variable {M}
+variable {F}
 
-lemma r_refl (x : M.PreDialation) : M.r x x := by sorry
+lemma prodElemPow_add (β γ : F^ℕ ) : 𝐚^(β + γ)= 𝐚^β* 𝐚^γ := by
+ simp[prodElemPower]
+ simp[pow_add, Finset.prod_mul_distrib,
+  Finset.prod_subset_one_on_sdiff, Finsupp.prod_add_index]
 
-lemma r_symm (x y : M.PreDialation) : M.r x y → M.r y x := by sorry
+omit [DecidableEq F.index] in
+lemma r_refl (x : F.PreDil) : F.r x x := by simp[r]
 
-lemma r_trans (x y z : M.PreDialation) : M.r x y → M.r y z → M.r x z := by sorry
+omit [DecidableEq F.index] in
+lemma r_symm (x y : F.PreDil) : F.r x y → F.r y x := by
+  intro h
+  rcases h with ⟨β , hβ⟩
+  use β
+  rw[hβ.symm]
 
-def setoid : Setoid (M.PreDialation) where
-  r := M.r
+lemma r_trans (x y z : F.PreDil) : F.r x y → F.r y z → F.r x z := by
+  intro h g
+  rcases h with ⟨β , hβ⟩
+  rcases g with ⟨γ , gγ⟩
+  have eq' := congr($hβ * 𝐚^(γ+z.pow))
+  have eq'' := congr($gγ * 𝐚^(β+x.pow))
+  use β+γ+y.pow
+  simp only [← prodElemPow_add, ← mul_assoc] at eq' eq'' ⊢
+  rw [show β + γ + y.pow + z.pow = (β + y.pow) + (γ + z.pow) by abel,
+    prodElemPow_add, ← mul_assoc, hβ, mul_assoc, mul_comm (𝐚^ _), ← mul_assoc, gγ,
+    mul_assoc, ← prodElemPow_add]
+  congr 2
+  abel
+
+def setoid : Setoid (F.PreDil) where
+  r := F.r
   iseqv :=
   { refl := r_refl
     symm {x y} := r_symm x y
     trans {x y z} := r_trans x y z }
 
-variable (M) in
-def Dilatation := Quotient M.setoid
+variable (F) in
+def Dilatation := Quotient F.setoid
 
-scoped notation:max A"["M"]" => Dilatation (A := A) M
+scoped notation:max A"["F"]" => Dilatation (A := A) F
 
-instance : Add A[M] where
+instance : Add A[F] where
   add := Quotient.map₂ sorry sorry
 
-instance : Mul A[M] where
+instance : Mul A[F] where
   mul := Quotient.map₂ sorry sorry
 
-instance : Zero A[M] where
+instance : Zero A[F] where
   zero := sorry
 
-instance : One A[M] where
+instance : One A[F] where
   one := sorry
 
-instance : AddCommMonoid A[M] where
+instance : AddCommMonoid A[F] where
   add_assoc := sorry
   zero_add := sorry
   add_zero := sorry
@@ -85,7 +108,7 @@ instance : AddCommMonoid A[M] where
   nsmul := nsmulRec
 
 
-instance instCommSemiring : CommSemiring A[M] where
+instance instCommSemiring : CommSemiring A[F] where
   left_distrib := sorry
   right_distrib := sorry
   zero_mul := sorry
@@ -99,12 +122,12 @@ end semiring
 
 section ring
 
-variable {A : Type*} [CommRing A] (M : Multicenter A) [DecidableEq M.index]
+variable {A : Type*} [CommRing A] (F : Multicenter A) [DecidableEq F.index]
 
-instance : Neg A[M] where
+instance : Neg A[F] where
   neg := Quotient.map sorry sorry
 
-instance : CommRing A[M] where
+instance : CommRing A[F] where
   __ := instCommSemiring
   zsmul := zsmulRec
   neg_add_cancel := sorry
