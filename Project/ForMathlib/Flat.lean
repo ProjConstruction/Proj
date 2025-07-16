b2619ebbf0060ef3ed3f3be845274ef6ad7e8cb6
jjaassoonn <jujian.zhang1998@outlook.com>
2025-07-16 23:41:35 +0100

    fix

--- Project/ForMathlib/Flat.lean
+++ Project/ForMathlib/Flat.lean
@@ -2,7 +2,7 @@ import Mathlib.RingTheory.RingHom.Flat
 
 #check RingHom.Flat
 
-lemma RingHom.Flat.preserves_nonzeroDivisors {R S F : Type*} [CommRing R] [CommRing S] {f : R →+* S}
+lemma RingHom.Flat.preserves_nonzeroDivisors {R S : Type*} [CommRing R] [CommRing S] {f : R →+* S}
     (flat : RingHom.Flat f) {r : R} (hr : r ∈ nonZeroDivisors R) :
     f r ∈ nonZeroDivisors S := by
   algebraize [f]
